d1ab8fb078880325f04bb73a8acd41d072f42f6e
stc <liyonghelpme@gmail.com>
2013-03-20 13:59:47 +0800

    Initial Commit

---
+++ libav2/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  libav2AppController.h
+//  libav2
+//
+//  Created by  stc on 13-3-20.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ libav2/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  libav2AppController.h
+//  libav2
+//
+//  Created by  stc on 13-3-20.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
